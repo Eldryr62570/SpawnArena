f76179d0dcda5eca360cca612dffff21364d9fa4
Eldryr62570 <jordan.moulin62570@gmail.com>
2022-01-25 14:33:36 +0100

    l

--- mailer.php
+++ mailer.php
@@ -6,6 +6,8 @@
     $headers[]= 'MIME-Version: 1.0'; 
     $headers[]= 'Content-type: text/html; charset=iso-8859-1';
     mail($to, $subject, $message, implode("\r\n", $headers));
+
+    
     $_SESSION["mysession"] = true;
     header("Location:presentation.php");
 ?>
