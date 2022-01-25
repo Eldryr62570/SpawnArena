f4824164bd0ca87fcf50bd04c072bd947af51fe4
Eldryr62570 <jordan.moulin62570@gmail.com>
2022-01-25 16:37:59 +0100

    début bdd

--- mailer.php
+++ mailer.php
@@ -2,6 +2,25 @@
     session_start();
     $to = 'jordan.moulin62570@gmail.com';
     $subject = $_POST["subject"];
+    $tel = $_POST["tel"];
+    $age = $_POST["age"];
+    $message = $_POST["message"];
+    $mail = $_POST["mail"];
+   
+    try{
+        $db = new PDO(
+            'mysql:host=localhost;dbname=spawnarena;charset=utf8',
+            'root',
+        );
+    }
+    catch(Exeption $e){
+        die('Erreur : ' . $e->getMessage());
+    }
+
+
+    $query = 'INSERT INTO'
+
+    /*
     $message = '<h1> Problème du client :'.$_POST["message"].'</h1>';
     $headers[]= 'MIME-Version: 1.0'; 
     $headers[]= 'Content-type: text/html; charset=iso-8859-1';
@@ -11,6 +30,6 @@
     if(isset($_SESSION)){
         $_SESSION["mysession"] = true;
     }
-        
+        */
     header("Location:presentation.php");
 ?>
